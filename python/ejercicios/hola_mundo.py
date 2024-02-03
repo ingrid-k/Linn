f7ee3a34f6b043ac188b37be7beb042df1be3d23
ingrid-k <ingridkaufmannok@gmail.com>
2024-02-03 16:42:35 -0300

    Agregar ejercicio hola_mundo

---
+++ python/ejercicios/hola_mundo.py
@@ -0,0 +1,28 @@
+#Crear un programa que imprima este patrón.
+
+#       1
+#     2 3
+#   4 5 6
+#7 8 9 10
+
+print("    1")
+print("   23")
+print("  456")
+print("78910")
+
+#Y este otro. 
+
+#DDDD   L
+#D   D  L
+#D   D  L
+#D   D  L
+#D   D  L
+#D   D  L
+#DDDD   LLLLL
+
+print("DDD  L")
+print("D  D L")
+print("D  D L")
+print("D  D L")
+print("D  D L")
+print("DDDD LLLL")
