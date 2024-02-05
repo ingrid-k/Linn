7ba26908567eb44881c98446191447ba0544ddf9
ingrid-k <ingridkaufmannok@gmail.com>
2024-02-05 18:46:43 -0300

    Agregar ejercicio estructuras

--- python/ejercicios/estructuras_de_control.py
+++ python/ejercicios/estructuras_de_control.py
@@ -68,3 +68,69 @@ elif height >= 1.37 and credits < 10:
     print("No tienes suficientes creditos")
 else:
     print("No has cumplido con los requisitos")
+
+#Sombrero seleccionador
+
+gryffindor = 0
+hufflepuff = 0
+ravenclaw = 0
+slytherin = 0
+
+
+q1= print("Do you like Dawn or Dusk? Dawn or Dusk")
+respuesta_1 = int(input('Elija 1 o 2: '))
+
+if respuesta_1 == 1:
+    gryffindor += 1
+    ravenclaw += 1
+elif respuesta_1 == 2:
+    hufflepuff += 1
+    slytherin +=1
+else:
+    print('Respuesta incorrecta')
+
+
+q2= print("When Im dead, I want people to remember me as: 1) The Good 2) The Great 3) The Wise 4) The Bold")
+respuesta_2 = int(input('Elija un numero entre 1 y 4: '))
+
+if respuesta_2 == 1:
+    hufflepuff += 2
+elif respuesta_2 == 2:
+    slytherin += 2
+elif respuesta_2 == 3:
+    ravenclaw += 2
+elif respuesta_2 == 4:
+    gryffindor += 2
+else:
+    print('Respuesta incorrecta')
+
+
+q3= print("Which kind of instrument most pleases your ear? 1) The violin 2) The trumpet 3) The piano 4) The drum")
+respuesta_3 = int(input('Elija un numero entre 1 y 4: '))
+
+if respuesta_3 == 1:
+    slytherin += 4
+elif respuesta_3 == 2:
+    hufflepuff += 4
+elif respuesta_3 == 3:
+    ravenclaw +=4
+elif respuesta_3 == 4:
+    gryffindor += 4
+else:
+    print('Respuesta incorrecta')
+
+print("Gryffindor: ", gryffindor)
+print("Ravenclaw: ", ravenclaw)
+print("Hufflepuff: ", hufflepuff)
+print("Slytherin: ", slytherin)
+
+ganador = max(gryffindor, ravenclaw, hufflepuff, slytherin)
+
+if gryffindor == ganador:
+    print('¡Tu perteneces a: Gryffindor!')
+elif ravenclaw == ganador:
+    print('¡Tu perteneces a: Ravenclaw!')
+elif hufflepuff == ganador:
+    print('¡Tu perteneces a: Hufflepuff!')
+else:
+    print('¡Tu perteneces a: Slytherin!')
