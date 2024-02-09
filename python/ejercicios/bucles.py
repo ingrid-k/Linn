3c89f5fc2c3a1416c1894bc17860d0c5cedb126e
ingrid-k <ingridkaufmannok@gmail.com>
2024-02-09 12:07:25 -0300

    Agregar ejercicio bucles

--- python/ejercicios/bucles.py
+++ python/ejercicios/bucles.py
@@ -23,10 +23,25 @@ print("Lo adivinaste!")
 for i in range(41):
   print(i)
 
-#Programa que representa la canción "99 bottles of beer".
+#Crear un programa que represente la canción "99 bottles of beer".
 
 for i in range(99, 0, -1):
   print(f"{i} botellas de cerveza en la pared")
   print(f"{i} botellas de cerveza")
   print("toma una y pasala")
   print(f"{i-1} botellas de cerveza en la pared")
+
+#Crear un programa llamado "Fizz Buzz".
+#Multiplos de 3, imprime Fizz.
+#Multiplos de 5, imprime Buzz.
+#Multiplos de 3 y 5, imprime Fizz Buzz.
+
+for i in range(1, 101):
+  if i % 3 == 0 and i % 5 == 0:
+    print('FizzBuzz')
+  elif i % 3 == 0:
+    print('Fizz')
+  elif i % 5 == 0:
+    print('Buzz')
+  else:
+    print(i)
