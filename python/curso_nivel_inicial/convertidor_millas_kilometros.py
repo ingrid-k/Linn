149e7bc0af31e0d55471df2aae19c63559f2a855
ingrid-k <ingridkaufmannok@gmail.com>
2024-03-02 15:26:36 -0300

    Agregar ejercicio python

---
+++ python/curso_nivel_inicial/convertidor_millas_kilometros.py
@@ -0,0 +1,8 @@
+kilometros = 12.25
+millas = 7.38
+
+millas_a_kilometros = millas * 1.61
+kilometros_a_millas = kilometros / 1.61
+
+print(millas, "millas son", round(millas_a_kilometros, 2), "kilómetros")
+print(kilometros, "kilómetros son", round(kilometros_a_millas, 2), "millas")
